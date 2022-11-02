fcd332e1e5b8d176ceab1fc1934c8c54d0006eff
NorthNoah <1264918006@qq.com>
2022-11-02 11:15:59 +0800

    11.2

--- 1 Vue基础学习/NH-TRIP/src/main.js
+++ 1 Vue基础学习/NH-TRIP/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "normalize.css"
+import "./assets/index.css"
+import router from '@/router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ 1 Vue基础学习/NH-TRIP/src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+    // 历史记录：哈希写法
+    history: createWebHashHistory(),
+
+    // 映射关系:path -> component
+    routes: [
+
+    ]
+})
+
+export default router
